368922c537b572c3505f3e70173b4b0be5105f86
Talesb <talesiago@gmail.com>
2021-07-13 17:56:08 -0400

    Arquivos dos sensores
    Adição dos exemplos de uso de cada sensor e do arquivo completo

--- espGiro+UDP/esp/esp.ino
+++ espGiro+UDP/esp/esp.ino
@@ -6,6 +6,17 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <Ultrasonic.h> 
+/* Pinagem
+ * ---------------------    
+ * | HC-SC04 | NodeMCU |   
+ * ---------------------   
+ * |   Vcc   |   5V    |     
+ * |   Trig  |   D7    | 
+ * |   Echo  |   D8    |   
+ * |   Gnd   |   GND   |   
+ */
+Ultrasonic ultrasonic(D7, D8);
 
 String wifiName = "Tales07";
 String wifiPass = "S3m_S3nh@";
@@ -52,67 +63,6 @@ void setup() {
       delay(10);
     }
   }
-  Serial.println("MPU6050 Found!");
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-    case MPU6050_RANGE_2_G:
-      Serial.println("+-2G");
-      break;
-    case MPU6050_RANGE_4_G:
-      Serial.println("+-4G");
-      break;
-    case MPU6050_RANGE_8_G:
-      Serial.println("+-8G");
-      break;
-    case MPU6050_RANGE_16_G:
-      Serial.println("+-16G");
-      break;
-  }
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-    case MPU6050_RANGE_250_DEG:
-      Serial.println("+- 250 deg/s");
-      break;
-    case MPU6050_RANGE_500_DEG:
-      Serial.println("+- 500 deg/s");
-      break;
-    case MPU6050_RANGE_1000_DEG:
-      Serial.println("+- 1000 deg/s");
-      break;
-    case MPU6050_RANGE_2000_DEG:
-      Serial.println("+- 2000 deg/s");
-      break;
-  }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-    case MPU6050_BAND_260_HZ:
-      Serial.println("260 Hz");
-      break;
-    case MPU6050_BAND_184_HZ:
-      Serial.println("184 Hz");
-      break;
-    case MPU6050_BAND_94_HZ:
-      Serial.println("94 Hz");
-      break;
-    case MPU6050_BAND_44_HZ:
-      Serial.println("44 Hz");
-      break;
-    case MPU6050_BAND_21_HZ:
-      Serial.println("21 Hz");
-      break;
-    case MPU6050_BAND_10_HZ:
-      Serial.println("10 Hz");
-      break;
-    case MPU6050_BAND_5_HZ:
-      Serial.println("5 Hz");
-      break;
-  }
-
-
   digitalWrite(D4, LOW);
 }
 
@@ -148,13 +98,20 @@ void listen() {
         req+=a.acceleration.y;
         req+=", A.z: ";
         req+=a.acceleration.y;
-        req+=" R.x: ";
+        enviar(req);
+        hold(100 );
+        req="R.x: ";
         req+=g.gyro.x;
         req+=" R.y: ";
         req+=g.gyro.x;
         req+=" R.z: ";
         req+=g.gyro.x;
         enviar(req);
+        req = "Distancia: ";
+        req+= ultrasonic.distanceRead();
+        enviar(req);
+        hold(100);
+        
     }else if(comando[0] == "REDE"){
       enviar("oi");
     }
