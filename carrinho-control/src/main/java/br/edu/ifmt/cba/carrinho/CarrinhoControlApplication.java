a15a4c18424a10970b7d75a6f75ce6078644a9c9
Gabriel Honda <gabrielhondacba@gmail.com>
2021-07-19 19:33:06 -0400

    chore: adiciona projeto java com spring boot

---
+++ carrinho-control/src/main/java/br/edu/ifmt/cba/carrinho/CarrinhoControlApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.cba.carrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoControlApplication.class, args);
+	}
+
+}
