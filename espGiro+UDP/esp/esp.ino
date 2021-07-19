8a7c75fb5a3e32a814dd70f7b572b502bfa99bf0
Talesb <talesiago@gmail.com>
2021-07-19 18:21:38 -0400

    Atualizando
    Mudando a comunicação

--- espGiro+UDP/esp/esp.ino
+++ espGiro+UDP/esp/esp.ino
@@ -1,25 +1,39 @@
 #include <StringSplitter.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+#include <HTTPClient.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-#include <Ultrasonic.h> 
+#include <Ultrasonic.h>
+#include <Servo.h>
+
 /* Pinagem
- * ---------------------    
- * | HC-SC04 | NodeMCU |   
- * ---------------------   
- * |   Vcc   |   5V    |     
- * |   Trig  |   D7    | 
- * |   Echo  |   D8    |   
- * |   Gnd   |   GND   |   
- */
-Ultrasonic ultrasonic(D7, D8);
+   ---------------------
+   | Função  | Pino |
+   ---------------------
+   Sonar:
+   |   Trig  |   D3    |
+   |   Echo  |   D4    |
+   Giroscópio:
+   |   SCL   |   D1    |
+   |   SDA   |   D2    |
+   Servo:
+   |   Com   |   D8    |
+   Ponte H:
+   | MotorA1 |   D0    |
+   | MotorA2 |   D5    |
+   | MotorB1 |   D6    |
+   | MotorB2 |   D7    |
+*/
+
+Ultrasonic ultrasonic(D3, D4);
+Servo s;
 
 String wifiName = "Tales07";
 String wifiPass = "S3m_S3nh@";
+int medidas[180];
 
 Adafruit_MPU6050 mpu;
 
@@ -28,7 +42,7 @@ String req;
 
 unsigned int localPort = 8888;
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
-char cstr[50];
+char cstr[200];
 
 char _buffer[64];
 
@@ -63,6 +77,14 @@ void setup() {
       delay(10);
     }
   }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  s.attach(D8);
+  s.write(0);
+
   digitalWrite(D4, LOW);
 }
 
@@ -87,35 +109,34 @@ void listen() {
     }
 
     Serial.println("Comando recebido: " + req);
-    
 
-    if(comando[1]=="comando"){
-        sensors_event_t a, g, temp;
-        mpu.getEvent(&a, &g, &temp);
-        req = "A.x: ";
-        req+= a.acceleration.x;
-        req+=", y: ";
-        req+=a.acceleration.y;
-        req+=", A.z: ";
-        req+=a.acceleration.y;
-        enviar(req);
-        hold(100 );
-        req="R.x: ";
-        req+=g.gyro.x;
-        req+=" R.y: ";
-        req+=g.gyro.x;
-        req+=" R.z: ";
-        req+=g.gyro.x;
-        enviar(req);
-        req = "Distancia: ";
-        req+= ultrasonic.distanceRead();
-        enviar(req);
-        hold(100);
-        
-    }else if(comando[0] == "REDE"){
+
+    if (comando[1] == "comando") {
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+      req = "A.x: ";
+      req += a.acceleration.x;
+      req += ", y: ";
+      req += a.acceleration.y;
+      req += ", A.z: ";
+      req += a.acceleration.y;
+      enviar(req);
+      hold(100 );
+      req = "R.x: ";
+      req += g.gyro.x;
+      req += " R.y: ";
+      req += g.gyro.x;
+      req += " R.z: ";
+      req += g.gyro.x;
+      enviar(req);
+      hold(100);
+
+    } else if (comando[0] == "REDE") {
       enviar("oi");
+    } else if (comando[1] == "leitura") {
+
     }
-    
+
     req = "";
     hold(100);
   }
@@ -128,6 +149,24 @@ void hold(const unsigned int &ms) {
     yield();
   }
 }
+
+void leituraSonar() {
+  long microsec = ultrasonic.timing();
+  
+  for (int i = 0; i < 180; i++) {
+    s.write(i);
+    medidas[i] = (int)ultrasonic.convert(microsec, Ultrasonic::CM);
+    hold(100);
+  }
+  req = "";
+  for (int j = 0; j < 180; j++) {
+    req += medidas[j];
+    req += "/";
+  }
+  enviar(req);
+}
+
+
 
 void enviar(String msg) {
   String sMsgTemp = "PLACA/Carro/";
