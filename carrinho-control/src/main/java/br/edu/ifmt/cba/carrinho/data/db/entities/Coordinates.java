2ad18450c9b013de9c18b77e3de10f7816f9a79a
Gabriel Honda <gabriel.honda@totemti.com.br>
2021-07-24 10:48:38 -0400

    feat: adiciona modelagem inicial

---
+++ carrinho-control/src/main/java/br/edu/ifmt/cba/carrinho/data/db/entities/Coordinates.java
@@ -0,0 +1,37 @@
+package br.edu.ifmt.cba.carrinho.data.db.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * @author daohn
+ * @since 24/07/2021
+ */
+@Entity
+@Getter @Setter
+@Table(name = "COORDINATES")
+@AllArgsConstructor @NoArgsConstructor
+public class Coordinates extends AbstractEntity {
+
+  @Column(name = "x0")
+  private Double initialX;
+  @Column(name = "y0")
+  private Double initialY;
+
+  @Column(name = "xf")
+  private Double finalX;
+  @Column(name = "yf")
+  private Double finalY;
+
+  @ManyToOne
+  @JoinColumn(name = "id_map")
+  private Map map;
+}
---
+++ carrinho-control/src/main/java/br/edu/ifmt/cba/carrinho/data/db/entities/Map.java
@@ -0,0 +1,33 @@
+package br.edu.ifmt.cba.carrinho.data.db.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+/**
+ * @author daohn
+ * @since 24/07/2021
+ */
+@Entity
+@Getter @Setter
+@Table(name = "MAP")
+@AllArgsConstructor @NoArgsConstructor
+public class Map extends AbstractEntity {
+
+  @Column(name = "locality", unique = true)
+  private String locality;
+
+  @Column(name = "operator")
+  private String operator;
+
+  @OneToMany(mappedBy = "map")
+  private List<Coordinates> coordinates;
+
+}
